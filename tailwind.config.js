1d54cfffa9307df7f912fbc93802c6bded3c3384
egorsv1985 <egorvs2009@gmail.com>
2024-01-23 09:42:11 +0300

    start

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./**/*.html'],
+	theme: {
+		colors: {
+			red: '#E63C24',
+			blue: '#2B2B59',
+			white: '#fff',
+			dark: '#000',
+			green: '#13ce66',
+			yellow: '#ffc82c',
+			'gray-dark': '#273444',
+			gray: '#8492a6',
+			'gray-light': '#d3dce6',
+		},
+		fontFamily: {
+			lato: ['Lato', 'sans-serif'],
+		},
+		extend: {},
+	},
+	plugins: [],
+}
